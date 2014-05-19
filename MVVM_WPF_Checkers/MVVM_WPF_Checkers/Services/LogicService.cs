bae61d60300dbbbcc2bb077a6f4cabca0c7bbbe1
bartek.kaaz <bartek47@gmail.com>
2014-05-19 16:15:56 +0200

    new moves

--- MVVM_WPF_Checkers/MVVM_WPF_Checkers/Services/LogicService.cs
+++ MVVM_WPF_Checkers/MVVM_WPF_Checkers/Services/LogicService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Serialization;
 using MVVM_WPF_Checkers.Models;
 
 namespace MVVM_WPF_Checkers.Services
@@ -9,6 +10,8 @@ namespace MVVM_WPF_Checkers.Services
     {
         private FieldState[,] _boardArray;
         private FieldState[,] _previousBoardArray;
+        private List<FieldState[,]> _possibleMoves;
+        private FieldState[,] _possibleCaptures;
 
         public FieldState[,] BoardArray
         {
@@ -23,6 +26,8 @@ namespace MVVM_WPF_Checkers.Services
         {
             _boardArray = new FieldState[8, 8];
             _previousBoardArray = new FieldState[8, 8];
+            _possibleMoves = new List<FieldState[,]>();
+            _possibleCaptures = new FieldState[8, 8];
         }
 
 
@@ -56,15 +61,59 @@ namespace MVVM_WPF_Checkers.Services
 
         public string Validete()
         {
-            var diff = GetDiff();
-            string message = "Invalid move";
-            foreach (var pawn in diff)
-                message += String.Format(" {0}", pawn);
-            if (diff.Count == 0) return null;
-            if (diff.Count != 2) return message;
-            if (!ValidateMovePawn(diff)) return message;
+            //var diff = GetDiff();
+            //string message = "Invalid move";
+            //foreach (var pawn in diff)
+            //    message += String.Format(" {0}", pawn);
+            //if (diff.Count == 0) return null;
+            //if (diff.Count != 2) return message;
+            //if (!ValidateMovePawn(diff)) return message;
+
+            SetPossibleMoves();
 
             return null;
+        }
+
+        private void SetPossibleMoves()
+        {
+            _possibleMoves.Clear();
+
+            for(var i=0; i<8; i++)
+                for (var j = 0; j < 8; j++)
+                {
+                    var fieldState = _previousBoardArray[i, j];
+                    MovePawns(i, j, fieldState);
+                    MoveDames(i, j, fieldState);
+                }
+        }
+
+        private void MovePawns(int x, int y, FieldState fieldState)
+        {
+            if (fieldState != FieldState.RedPawn && fieldState != FieldState.YellowPawn) return;
+            var direction = 0;
+            if (fieldState == FieldState.RedPawn) direction = 1;
+            if (fieldState == FieldState.YellowPawn) direction = -1;
+            MovePawn(x, y, x + direction, y + 1);
+            MovePawn(x, y, x + direction, y - 1);
+        }
+
+        private void MoveDames(int x, int y, FieldState fieldState)
+        {
+            if (fieldState != FieldState.GreenPawn && fieldState != FieldState.BluePawn) return;
+            for (var i = 0; i < 8; i++) MovePawn(x, y, x + i, y + i);
+            for (var i = 0; i < 8; i++) MovePawn(x, y, x + i, y - i);
+            for (var i = 0; i < 8; i++) MovePawn(x, y, x - i, y - i);
+            for (var i = 0; i < 8; i++) MovePawn(x, y, x - i, y + i); 
+        }
+
+        private void MovePawn(int currentX, int currentY, int newX, int newY)
+        {
+            if (newX < 0 || newX > 8 || newY < 0 || newY > 8) return;
+            if (_previousBoardArray[newX, newY] != FieldState.Empty) return;
+            var tmpBoard = (FieldState[,])_previousBoardArray.Clone();
+            tmpBoard[newX, newY] = tmpBoard[currentX, currentY];
+            tmpBoard[currentX, currentY] = FieldState.Empty;
+            _possibleMoves.Add(tmpBoard);
         }
 
         private bool ValidateMovePawn(List<Pawn> diff)
@@ -77,8 +126,8 @@ namespace MVVM_WPF_Checkers.Services
             var x = newPawnPossition.X - oldPawnPossition.X;
             var y = newPawnPossition.Y - oldPawnPossition.Y;
 
-            if (!(newPawnPossition.CurrentState != FieldState.BluePawn || newPawnPossition.CurrentState != FieldState.GreenPawn))
-                if (Math.Abs(x) != 1) return false;
+            //if (!(newPawnPossition.CurrentState != FieldState.BluePawn || newPawnPossition.CurrentState != FieldState.GreenPawn))
+            if (Math.Abs(x) != 1) return false;
             if (Math.Abs(x) != Math.Abs(y)) return false;
             if (newPawnPossition.CurrentState == FieldState.RedPawn && newPawnPossition.X < oldPawnPossition.X) return false;
             if (newPawnPossition.CurrentState == FieldState.YellowPawn && newPawnPossition.X > oldPawnPossition.X) return false;
